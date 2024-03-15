61b238a7915bb89900adda550ed7e6f7b2b42e07
Safiq Faray <syafiqfaray2@gmail.com>
2024-03-15 13:45:11 +0700

    update setup

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
 from setuptools import setup, find_packages
 
-setup(name='ekphrasis',
-      version='0.5.5',
-      description='Text processing tool, geared towards text from '
-                  'social networks, such as Twitter or Facebook. '
-                  'Ekphrasis performs tokenization, word normalization, '
-                  'word segmentation (for splitting hashtags) '
-                  'and spell correction.',
-      url='https://github.com/cbaziotis/ekphrasis',
-      author='Christos Baziotis',
-      author_email='christos.baziotis@gmail.com',
-      license='MIT',
-      packages=find_packages(exclude=['docs', 'tests*', 'analysis']),
-      install_requires=[
-          'termcolor',
-          'tqdm',
-          'colorama',
-          'ujson',
-          'matplotlib',
-          'nltk',
-          'ftfy',
-          'numpy'
-      ],
-      include_package_data=True
-      )
+setup(
+    name="ekphrasis",
+    version="0.5.51",
+    description="Text processing tool, geared towards text from "
+    "social networks, such as Twitter or Facebook. "
+    "Ekphrasis performs tokenization, word normalization, "
+    "word segmentation (for splitting hashtags) "
+    "and spell correction.",
+    url="https://github.com/cbaziotis/ekphrasis",
+    author="Christos Baziotis",
+    author_email="christos.baziotis@gmail.com",
+    license="MIT",
+    packages=find_packages(exclude=["docs", "tests*", "analysis"]),
+    install_requires=[
+        "termcolor",
+        "tqdm",
+        "colorama",
+        "ujson",
+        "matplotlib",
+        "nltk",
+        "ftfy",
+        "numpy",
+        "git+https://github.com/Haruray/emot.git",
+    ],
+    include_package_data=True,
+)
